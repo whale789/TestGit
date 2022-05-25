dc901ef2ffb9cea48a154e2ef5eea1f4617e5c60
whale789 <1974396570@qq.com>
2022-05-25 17:19:20 +0800

    777

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -2,3 +2,6 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I am hekai");
 Console.WriteLine("KKKK");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("I am hekai");
+Console.WriteLine("KKKK");
